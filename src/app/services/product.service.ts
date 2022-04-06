36a47dfd947dc6d7d481fb4bd235095eb3167bc7
Eslam Magdy <eslam3400@gmail.com>
2022-04-07 00:25:55 +0200

    accordion

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,8 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class ProductService {
   products: any[] = [];
+  id: number = 0;
 
   add(_product: any) {
+    _product.id = ++this.id;
     this.products.push(_product);
   }
 }
