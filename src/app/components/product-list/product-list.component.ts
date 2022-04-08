6eee5bf47ddb2f195874d48b8063bdab38320ae4
Eslam Magdy <eslam3400@gmail.com>
2022-04-08 02:12:16 +0200

    search products

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,31 @@
 import { ProductService } from './../../services/product.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.sass']
 })
-export class ProductListComponent {
-  constructor(public ProductService: ProductService) { }
+export class ProductListComponent implements OnInit {
+  filteredProducts: any;
+  name: any;
+
+  constructor(private ProductService: ProductService) { }
+
+  ngOnInit(): void {
+    this.resetProducts();
+  }
+
+  resetProducts() {
+    this.filteredProducts = this.ProductService.products;
+  }
+
+  filterItem(value: string) {
+    if (!value) {
+      this.resetProducts();
+    }
+    this.filteredProducts = Object.assign([], this.ProductService.products).filter(
+      (product: any) => product.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+    )
+  }
 }
